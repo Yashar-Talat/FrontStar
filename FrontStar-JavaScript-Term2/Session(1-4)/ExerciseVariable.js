fec44e4bf01002da28eb74250cb2b98d60d0a5bf
Yashar <Yashar.talat@gmail.com>
2022-09-04 19:56:08 +0430

    LearnAll

---
+++ FrontStar-JavaScript-Term2/Session(1-4)/ExerciseVariable.js
@@ -0,0 +1,25 @@
+var Name = "Yashar";
+var lastName = "Talat";
+// alert(Name + " " + lastName);
+document.write(Name + " " +lastName);
+var Names = ["Yashar","Amin","Fatemeh","Elnaz"];
+document.write("<br />");
+
+document.write(Name.length);
+document.write("<br />");
+document.write(lastName);
+document.write("<br />");
+
+document.write(Names[2]);
+document.write("<br />");
+
+var NameSort = Names.sort();
+document.write(NameSort);
+document.write("<br />");
+
+// console.log(Names[4]);
+
+var person = ["Yashar","Talat"];
+document.write("First Name is:" + person[0] + "<br /> " + "Last Name is:" + person[1]);
+
+console.log(person[0]);

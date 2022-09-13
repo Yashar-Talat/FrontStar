7f335c0dafeda14b438df57a7a271e4e6c4aa2c0
Yashar <Yashar.talat@gmail.com>
2022-09-13 19:30:45 +0430

    Review

---
+++ Review.js
@@ -0,0 +1,33 @@
+// let names = ["Amin","Yashar",1,55,"Talat"];
+// document.write(names[1]);
+// console.log(names);
+
+// function Multiply() {
+//     let x = document.getElementById("number1").value;
+//     let y = document.getElementById("number2").value;
+//     x = parseInt(x);
+//     y = parseInt(y);
+//     z = x * y ;
+//     res.innerHTML = z;
+//
+// }
+// let person = {
+//     name : "Yashar",
+//     LastName : "Talat",
+//     age : 19,
+//     fullName : function () {
+//         return this.name + this.LastName ;
+//     },
+//     Talent : "Eng.Computer"
+// };
+// document.write(person.fullName() + "<br>");
+// document.write(person.Talent);
+debugger;
+
+function sum() {
+    var x = 2;
+    var y = 5;
+    return x + y;
+}
+document.getElementById("result").innerHTML = sum();
+// document.write(sum());

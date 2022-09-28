982a1e7a85b747e9ccbc6ffa29dd6cd7c93aaf19
Yashar <Yashar.talat@gmail.com>
2022-09-28 11:37:04 +0330

    Get-Single-Post

--- FrontStar-JavaScript-Term2/Session(37_40)/EasyHTTP2.js
+++ FrontStar-JavaScript-Term2/Session(37_40)/EasyHTTP2.js
@@ -1,17 +1,18 @@
-class EasyHTTP2 {
+class EasyHttp {
     constructor() {
         this.http = new XMLHttpRequest();
     }
 
     getPosts(url,callback) {
-       this.http.open("GET",url,true);
-       this.http.onload = () => {
-        if(this.http.status === 200){
-            callback(null, this.http.responseText);
-        }else{
-            callback(`Error : ${this.http.status}`);
+        this.http.open("GET",url,true);
+        this.http.onload = () => {
+            if(this.http.status === 200) {
+                callback(null,this.http.responseText);
+            }
+            else {
+                callback('Error :'`${this.http.status}`);
+            }
         }
-       }
-       this.http.send();
+        this.http.send();
     }
 }
--- FrontStar-JavaScript-Term2/Session(37_40)/GetSinglePost.js
+++ FrontStar-JavaScript-Term2/Session(37_40)/GetSinglePost.js
@@ -1,9 +1,8 @@
-let http = new EasyHTTP2();
-
-let posts = http.getPosts('https://jsonplaceholder.typicode.com/posts/5',function(err,posts) {
-    if(err) {
-        console.log(err);
-    }else {
-        console.log(posts);
-    }
+let http = new EasyHttp();
+let posts = http.getPosts("https://jsonplaceholder.typicode.com/posts/10",function(err,posts){
+ if(err) {
+    console.log(err);
+ }else {
+    console.log(posts);
+ }
 });

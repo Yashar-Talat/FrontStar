33687076aa3536d9975239cb1ae9855d324a5ff4
Yashar <Yashar.talat@gmail.com>
2022-10-06 01:04:31 +0330

    EasyHttpWithFetch

--- FrontStar-JavaScript-Term2/Session(41-43)/ApiFetch.js
+++ FrontStar-JavaScript-Term2/Session(41-43)/ApiFetch.js
@@ -1,23 +1,23 @@
 let http = new EasyHttpFetch();
-// http.get("https://jsonplaceholder.typicode.com/users")
-//     .then(users => {
-//         console.log(users)
-//     }).catch(err => {
-//     console.log(err)
-// });
+http.get("https://jsonplaceholder.typicode.com/users")
+    .then(users => {
+        console.log(users)
+    }).catch(err => {
+    console.log(err)
+});
 let user = {name: "Yashar", userName: "YasharTalat", Age: "19"};
 
-// http.post("https://jsonplaceholder.typicode.com/users",user).then(user => {
-//     console.log(user)
-// }).catch(err => {
-//     console.log(err);
-// });
+http.post("https://jsonplaceholder.typicode.com/users",user).then(user => {
+    console.log(user)
+}).catch(err => {
+    console.log(err);
+});
 
-// http.put("https://jsonplaceholder.typicode.com/users/5",user).then(user => {
-//     console.log(user)
-// }).catch(err => {
-//     console.log(err);
-// });
+http.put("https://jsonplaceholder.typicode.com/users/5",user).then(user => {
+    console.log(user)
+}).catch(err => {
+    console.log(err);
+});
 
 http.delete("https://jsonplaceholder.typicode.com/users/5")
     .then(response => {
--- FrontStar-JavaScript-Term2/Session(41-43)/EasyHttpFetch.js
+++ FrontStar-JavaScript-Term2/Session(41-43)/EasyHttpFetch.js
@@ -1,45 +1,45 @@
 class EasyHttpFetch {
-    // get(url) {
-    //     return new Promise((resolve, reject) => {
-    //         fetch(url)
-    //             .then((res) => {
-    //                 return res.json();
-    //             }).then((data) => {
-    //             console.log(data);
-    //         }).catch((err) => {
-    //             console.log(err);
-    //         })
-    //     })
-    // }
-    // post(url,data) {
-    //     return new Promise((resolve,reject)=>{
-    //         fetch(url,{
-    //             method:"POST",
-    //             header: {
-    //                 "Content-type":"application/json"
-    //             },
-    //             body:JSON.stringify(data)
-    //         })
-    //             .then(res=> res.json())
-    //             .then(data=> resolve(data))
-    //             .catch(err=> reject(err))
-    //     });
-    // }
+    get(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url)
+                .then((res) => {
+                    return res.json();
+                }).then((data) => {
+                console.log(data);
+            }).catch((err) => {
+                console.log(err);
+            })
+        })
+    }
+    post(url,data) {
+        return new Promise((resolve,reject)=>{
+            fetch(url,{
+                method:"POST",
+                header: {
+                    "Content-type":"application/json"
+                },
+                body:JSON.stringify(data)
+            })
+                .then(res=> res.json())
+                .then(data=> resolve(data))
+                .catch(err=> reject(err))
+        });
+    }
 
-    // put(url,data) {
-    //     return new Promise((resolve,reject)=>{
-    //         fetch(url,{
-    //             method:"PUT",
-    //             header: {
-    //                 "Content-type":"application/json"
-    //             },
-    //             body:JSON.stringify(data)
-    //         })
-    //             .then(res=> res.json())
-    //             .then(data=> resolve(data))
-    //             .catch(err=> reject(err))
-    //     });
-    // }
+    put(url,data) {
+        return new Promise((resolve,reject)=>{
+            fetch(url,{
+                method:"PUT",
+                header: {
+                    "Content-type":"application/json"
+                },
+                body:JSON.stringify(data)
+            })
+                .then(res=> res.json())
+                .then(data=> resolve(data))
+                .catch(err=> reject(err))
+        });
+    }
     delete(url) {
         return new Promise((resolve,reject)=>{
             fetch(url)

12a638c12053350be9b9b015b260679e14813751
Yashar <Yashar.talat@gmail.com>
2022-10-05 20:29:14 +0330

    FetchComplete

--- FrontStar-JavaScript-Term2/Session(41-43)/Fetch.js
+++ FrontStar-JavaScript-Term2/Session(41-43)/Fetch.js
@@ -1,45 +1,46 @@
 document.querySelector("#button1").addEventListener("click", getText);
-document.querySelector("#button2").addEventListener("click", getJSON);
-document.querySelector("#button3").addEventListener("click", getAPI);
+document.querySelector("#button2").addEventListener("click", getJson);
+document.querySelector("#button3").addEventListener("click", getApi);
+
 
 
 
 function getText() {
     fetch("text.txt").then((res) => {
         return res.text();
-    }).then((data) => {
+    }).then(function (data) {
         console.log(data);
-    }).catch(function (err) {
+    }).catch((err) => {
         console.log(err);
     });
 }
 
-function getJSON() {
+function getJson() {
     fetch("Fetch.json").then((res) => {
         return res.json();
     }).then((data) => {
         let output = "";
         data.forEach(post => {
-            output += `<li>Name :${post.Name}</li>`;
-            output += `<li>Email :${post.Email}</li>`;
-            output += `<li>Age :${post.Age}</li>`;
+            output += `<li>${post.Name}</li>`;
+            output += `<li>${post.Age}</li>`;
+            output += `<li>${post.Email}</li>`;
         });
         document.querySelector(".output").innerHTML = output;
     }).catch((err) => {
         console.log(err);
-    });
+    })
 }
 
-function getAPI() {
-    fetch("http://api.github.com/users").then((res) => {
+function getApi() {
+    fetch("http://api.github.com/users").then((res)=>{
         return res.json();
-    }).then(function (data) {
+    }).then((data)=>{
         let output = "";
         data.forEach(user => {
-            output += `<li>${user.login}</li>`;
+            output += `<li>${user.login}</li>`
         });
         document.querySelector(".output").innerHTML = output;
-    }).catch((err) => {
+    }).catch((err)=>{
         console.log(err);
     });
 }
